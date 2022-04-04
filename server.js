7777e6627ef8d1bbabead157e4e14bb1d7279f92
Max Finkenbeiner <48648552+Runlay@users.noreply.github.com>
2022-04-04 11:48:34 +0200

    require

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // BASE SETUP
 // =============================================================================
-import cors from 'cors';
+var cors = require('cors');
 
 // call the packages we need
 var express = require('express'); // call express
