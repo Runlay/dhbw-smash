1cd9b072d1427aa47bd62e7ab8186ddabb13adbe
Max Finkenbeiner <48648552+Runlay@users.noreply.github.com>
2022-04-04 11:45:55 +0200

    move cors up

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ import cors from 'cors';
 // call the packages we need
 var express = require('express'); // call express
 var app = express(); // define our app using express
+
+app.use(cors());
+
 var bodyParser = require('body-parser');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+
 
 var port = process.env.PORT || 8080; // set our port
 
