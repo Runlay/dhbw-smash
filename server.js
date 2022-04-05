02388774fd734921094137270ea18668b6c8d75f
Max Finkenbeiner <runlaysnetwork@gmail.com>
2022-04-05 15:02:57 +0200

    random

--- server.js
+++ server.js
@@ -130,15 +130,10 @@ const data = [{
 router.get('/', function(req, res) {
     return res.json({ message: 'Tinder DHBW Web App!' });
 });
+
 // return random profile  
-age=Math.floor(Math.random() * 40);
 router.get('/profiles', function(req, res) {
-    return res.json({name:'Claire-Ann Lage',
-            age: Math.floor(Math.random() * 40),
-            ort:'Karlsruhe',
-            studiengang:'International Management',
-            link:'https://c6oxm85c.cloudimg.io/width/700/png-lossless.fgaussian0.foil1/https://az617363.vo.msecnd.net/imgmodels/models/MD10002949/r1-07980-00136be87ce8b78f472bf44518b096612dcd.jpg',
-            hobbies:'kochen, reiten'} );
+    return res.json(data[Math.floor(Math.random() * datasize)]);
 });
 
 router.get('/ranking', function(req, res) {
