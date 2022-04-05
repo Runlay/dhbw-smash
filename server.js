f06b471204633d28f36337b076c105053340030b
Max Finkenbeiner <runlaysnetwork@gmail.com>
2022-04-05 17:25:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var router = express.Router(); // get an instance of the express Router
 
 // temp data
 const data = [{
-    id:"1",
+    id:1,
     name: 'Claire-Ann Lage',
     age: '28',
     ort: 'Karlsruhe',
@@ -34,10 +34,10 @@ const data = [{
     link: 'https://c6oxm85c.cloudimg.io/width/700/png-lossless.fgaussian0.foil1/https://az617363.vo.msecnd.net/imgmodels/models/MD10002949/r1-07980-00136be87ce8b78f472bf44518b096612dcd.jpg',
     hobbies: 'kochen, reiten',
     standort: 'Karlsruhe',
-    clicked: '5'
+    clicked: 5,
     },
     {
-        id:"2",
+        id:2,
     name: 'Hella Kot',
     age: '36',
     ort: 'Pforzheim',
@@ -46,10 +46,10 @@ const data = [{
     link: 'https://1.bp.blogspot.com/-NGcQZkEKqmE/Xp8V_afb-cI/AAAAAAAACas/XQ07iFaIQwMIeX6phjYT9o4obdxDqlUUgCLcBGAsYHQ/s1600/Elena%2B101.jpeg',
     hobbies: 'hacken, chatten',
     standort: 'Karlsruhe',
-    clicked: '10'
+    clicked: 0,
     },
     {
-        id:"3",
+        id:3,
     name: 'Rainer Deutschmann',
     age: '45',
     ort: 'Stuttgart',
@@ -58,10 +58,10 @@ const data = [{
     link: 'https://mytie.info/wp-content/uploads/2019/05/frisuren-fur-manner-ab-50-jahren-20-besten-ideen-trendfrisuren-herren-frisuren-ab-50-jahre-2019-mogen-of-frisuren-fur-manner-ab-50-jahren.jpg',
     hobbies: 'lesen',
     standort: 'Stuttgart',
-    clicked: '2'
+    clicked: 2,
     },
     {
-        id:"4",
+        id:4,
     name: 'Sergey Fehrlich',
     age: '56',
     ort: 'Lörrach',
@@ -70,10 +70,10 @@ const data = [{
     link: 'https://www.bremer-dachdecker.de/images/aktuell/2018_tuedel_3.jpg',
     hobbies: 'hacken',
     standort: 'Lörrach',
-    clicked: '0'
+    clicked: 0,
     },
     {
-        id:"5",
+        id:5,
     name: 'Hans-Jürgen Höpfner',
     age: '50',
     ort: 'Ispringen',
@@ -82,10 +82,10 @@ const data = [{
     link: 'https://archzine.net/wp-content/uploads/2015/11/Johnny-Depp-Brille-Kleidung-Hut-hipster-style.jpg',
     hobbies: 'brauen, golfen',
     standort: 'Stuttgart',
-    clicked: '6'
+    clicked: 6,
     },
     {
-        id:"6",
+        id:6,
     name: 'Klaus Uhr',
     age: '40',
     ort: 'Mannheim',
@@ -94,10 +94,10 @@ const data = [{
     link: 'https://archzine.net/wp-content/uploads/2015/11/Ryan-Gosling-schwarze-Jacke-symoatisches-Modell-hornbrille.jpg',
     hobbies: 'gamen',
     standort: 'Mannheim',
-    clicked: '7'
+    clicked: 7,
     },
     {
-        id:"7",
+        id:7,
     name: 'Jürgen Jochen',
     age: '25',
     ort: 'Ulm',
@@ -106,10 +106,10 @@ const data = [{
     link: 'https://archzine.net/wp-content/uploads/2015/11/Junge-Jeans-blauer-Pullover-Halstuch-nerd-brille-schwarze-Rahmen.jpg',
     hobbies: 'kassierer im verein, lotto spielen',
     standort: 'Ulm',
-    clicked: '1'
+    clicked: 1,
     },
     {
-        id:"8",
+        id:8,
     name: 'Franz Ohse',
     age: '30',
     ort: 'Straßburg',
@@ -118,10 +118,10 @@ const data = [{
     link: 'https://archzine.net/wp-content/uploads/2015/11/Hipster-Brille-schlichtes-Modell-h%C3%BCbscher-Mann.jpg',
     hobbies: 'backen, surfen',
     standort: 'Lörrach',
-    clicked: '5'
+    clicked: 5,
     },
     {
-        id:"9",
+        id:9,
     name: 'Jana Türlich',
     age: '29',
     ort: 'Villingen',
@@ -130,7 +130,7 @@ const data = [{
     link: 'https://archzine.net/wp-content/uploads/2015/11/sympatisches-M%C3%A4dchen-Hipster-Look-hornbrille-runde-Form.jpg',
     hobbies: 'schwimmen, schießverein, bmx',
     standort: 'Villingen-Schwenningen',
-    clicked: '3'
+    clicked: 3,
     }
     ]
 
